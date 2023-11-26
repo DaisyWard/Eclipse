42d8b3c74d843d3aed0f9d4503b53d7bd0687908
Daisy Ward <daisydaisyemily@yahoo.co.uk>
2023-11-26 21:07:53 +0000

    Make low stock a constant

--- src/app/components/Stock/Stock.tsx
+++ src/app/components/Stock/Stock.tsx
@@ -4,13 +4,15 @@ import styles from '@styles/components/Stock.module.scss'
 
 import { StockComponentProps } from '@context/Shop.types'
 
+import { lowStockThreshold } from '../../utils/constants'
+
 const Stock: FC<StockComponentProps> = ({ stock, isRecommended }): JSX.Element => {
   return (
     <div>
       <p className={`${styles.stock} ${isRecommended && styles.recommendedProduct}`}>
-        {stock > 50 ? 'In stock' : 'Last few left'}
+        {stock > lowStockThreshold ? 'In stock' : 'Last few left'}
       </p>
-      {stock > 50 ?
+      {stock > lowStockThreshold ?
         <div className={styles.inStock}></div>
       :
         <>
